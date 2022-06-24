0a40dcd60aa74381c12c8b465553fd061e5f6d23
Ian Meza <ian.mezza@gmail.com>
2022-06-24 12:11:28 -0500

    Extracting get remaining balances

--- src/com/ianmeza/MortgageReport.java
+++ src/com/ianmeza/MortgageReport.java
@@ -3,20 +3,17 @@ package com.ianmeza;
 import java.text.NumberFormat;
 
 public class MortgageReport {
-    public static void displayPaymentSchedule(MortgageCalculator mortgageCalculator, int numberOfYears) {
-        int p = 1;
+    private static final NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+    public static void displayPaymentSchedule(MortgageCalculator mortgageCalculator) {
         var console = new Console();
-        String result = "PAYMENT SCHEDULE\n________________";
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        while (p <= numberOfYears * Main.MONTHS_IN_A_YEAR) {
-            result = result + "\n" + currency.format(mortgageCalculator.calculateRemainingBalance(p));
-            p ++;
+        console.log("PAYMENT SCHEDULE\n________________");
+        for (double balance: mortgageCalculator.getRemainingBalances()) {
+            console.log(currency.format(balance));
         }
-        console.log(result);
     }
 
     public static void displayMortgage(double mortgage) {
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
 //        format currency
         String formattedMortgage = currency.format(mortgage);
 //        print result
