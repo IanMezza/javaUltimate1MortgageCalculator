3eb283db42315909e3af332573cd2380a73a376c
Ian Meza <ian.mezza@gmail.com>
2022-09-24 08:42:49 -0500

    Add interface

--- src/com/ianmeza/Main.java
+++ src/com/ianmeza/Main.java
@@ -2,8 +2,6 @@ package com.ianmeza;
 
 public class Main {
     public static void main(String[] args) {
-        var control = new TextBox();
-        control.disable();
-        System.out.println(control.toString() );
+
     }
 }
